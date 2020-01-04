77055499ac2e0119f54f2600cb05ba93fab68396
SHalintHa <subhashana11@gmail.com>
2020-01-05 00:04:46 +0530

    Initial commit (reverse an integer without using string functions)

---
+++ ReverseAnInt.java
@@ -0,0 +1,24 @@
+public class ReverseAnInt{
+
+    static void reverseInteger(int n){
+
+        if(n == 0){
+            return;
+        }
+        else{
+            int rev = n % 10;
+            System.out.print(rev);
+            int temp_n = (int) n / 10;
+            reverseInteger(temp_n);
+        }
+        
+    }
+
+    public static void main(String[] args){
+
+        int num = 23406780;
+        reverseInteger(num);
+        System.out.println();
+
+    }
+}
