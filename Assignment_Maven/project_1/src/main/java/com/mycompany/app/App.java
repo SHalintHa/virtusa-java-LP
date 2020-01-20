7acbbb439053db0f5fd5b3f0db3ca9326d63a233
SHalintHa <subhashana011@gmail.com>
2020-01-20 05:40:58 +0530

    Updated Maven project

--- Assignment_Maven/project_1/src/main/java/com/mycompany/app/App.java
+++ Assignment_Maven/project_1/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        sayHello();
     }
+    
+
+	public static void sayHello() {
+		System.out.println("project_1");
+	}
 }
