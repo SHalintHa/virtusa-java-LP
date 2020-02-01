042f351c558080c7587b6be56c489d72945a1ca0
SHalintHa <subhashana011@gmail.com>
2020-02-01 19:12:09 +0530

    Update the parent_project

---
+++ Assignment_Maven/parent_project/src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld{
+
+
+	public static void printer(){
+
+		System.out.println("This is from Parent Parent Project");
+	}
+
+	public static void sayHello(){
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println("This is from Parent Parent Project");
+	}
+}
