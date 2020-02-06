49607fe5abaab1652cc5b74518833771b47c3db2
SHalintHa <subhashana011@gmail.com>
2020-02-07 02:08:31 +0530

    student RequestMapping json

--- Assignment_SpringBoot/student-service/src/main/java/com/shalintha/studentservice/controller/StudentController.java
+++ Assignment_SpringBoot/student-service/src/main/java/com/shalintha/studentservice/controller/StudentController.java
@@ -1,12 +1,15 @@
 package com.shalintha.studentservice.controller;
 
+import java.awt.PageAttributes.MediaType;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.shalintha.studentservice.model.Student;
 import com.shalintha.studentservice.model.StudentModel;
+
 
 @RestController
 public class StudentController {
@@ -16,6 +19,7 @@ public class StudentController {
 		
 		return "<h1>Hello World!</h1>";
 	}
+	
 	
 	@RequestMapping("/students")
 	public List<Student> getAllStudents(){
